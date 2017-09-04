70b76cf498c81f874fd868ad2180c9e5167c5dd4
mamingxing <mamingxing@192.168.100.126>
2017-09-04 11:17:20 +0800

    refactor

--- src/main/java/com/hticto/demo/module/api/controller/DemoController.java
+++ src/main/java/com/hticto/demo/module/api/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.hticto.demo.module.api.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class DemoController extends BaseController {
     private final static Logger logger = LoggerFactory.getLogger(DemoController.class); 
 
 	@RequestMapping("/test")
-	public Response home() {
+	public Response home(Model model) {
 		DemoData demo = new DemoData();
 		demo.setId(10);
 		demo.setName("threedong");
